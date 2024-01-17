338071f5ea105b52c12e198f20ed5eeb61c7462d
avenable11 <48918177+avenable11@users.noreply.github.com>
2024-01-17 23:14:13 +0000

    Final demo 1/17/2024

---
+++ 01_17_24/main.py
@@ -0,0 +1,22 @@
+"""First Demo program writen 
+1/17/2024 by Angela Venable"""
+
+print("Hello World") #printing hello world because it is the  
+                     #first line of code written for SDEV140
+print('A')
+print (3+4)
+name = input("Enter your name: ")
+print(name)
+print("Hello World, " + name)
+
+length = float(input("What is the length? "))
+width = float(input("What is the width? "))
+area = length * width
+print("The area of the rectangle is ", length*width)
+
+print("""This very long sentence extends
+all the way to the next line""")
+print("*" * 10)
+print(5/2)
+
+
